8974ad9566d03cb2eae184769355441343153ab6
anurag-inias <anurag.inias@gmail.com>
2019-08-15 02:56:38 +0530

    Fix update script

--- update-data.sh
+++ update-data.sh
@@ -3,4 +3,4 @@
 rm -f randomUsers.js
 touch randomUsers.js
 echo 'module.exports = ' > randomUsers.js
-curl -s curl -s https://randomuser.me/api?results=5000 >> randomUsers.js
+curl -s https://randomuser.me/api?results=5000 >> randomUsers.js
